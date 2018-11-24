52b1fd5174c7c3b57a2cdbe0cb3529539d6229f8
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-11-24 16:53:19 +0100

    Added remote origin url

--- Get-RepositorySummary.ps1
+++ Get-RepositorySummary.ps1
@@ -6,4 +6,39 @@
     stho, 11/2018
 
 #>
-Write-Host "Hi there!"
+
+function Get-RepositorySummary{
+    <#
+        .SYNOPSIS
+        Gets some information about the folder you are currently
+        in showing some information. 
+
+        .DESCRIPTION
+        
+          - Is it a git repository?
+          - Which remotes does it contain?
+          - What status is it in?
+
+        .EXAMPLE
+
+        Get-RepositorySummary .
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    Begin {}
+    Process {
+        $Output = New-Object PSCustomObject
+    
+        if ( Test-Path "$Path/.git" ) {
+            Add-Member -InputObject $Output -MemberType NoteProperty -Name VCS -Value "git"
+            $remoteOrigin = (git remote get-url origin)
+            Add-Member -InputObject $Output -MemberType NoteProperty -Name RemoteOrigin -Value $remoteOrigin
+        }
+        
+        $Output 
+    }
+    End {}
+}
