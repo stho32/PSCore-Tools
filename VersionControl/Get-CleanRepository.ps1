f98e7c46a51a5cd4e675fe39dd0a9e2fbd6dbf5f
Stefan Hoffmann <stho32github@gmail.com>
2018-12-09 18:41:25 +0100

    New Cmdlet to list all clean repositories within a directory

---
+++ VersionControl/Get-CleanRepository.ps1
@@ -0,0 +1,25 @@
+﻿function Get-CleanRepository {
+    <#
+        .SYNOPSIS
+        Gets a list of paths of all clean repositories in the current path.
+
+        .DESCRIPTION
+        The CmdLet checks all repositories for needed commits and pushs.
+        If there aren't any, it will return the path. 
+        This can be passed to another cmdlet like Remove-Item to e.g. clean up
+        the workspace.
+
+        .EXAMPLE
+        Get-CleanRepository | Remove-Item -Force -Recurse
+    #>
+    [CmdletBinding()]
+    param (
+    )
+    
+    process {
+        Get-RepositorySummary | 
+            Where-Object { ($_.HasUncommitedChanges -eq $false) -and
+                           ($_.UsePush -eq $false) } | 
+            Select-Object -ExpandProperty Path
+    }
+}
