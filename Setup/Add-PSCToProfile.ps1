88fb012d20b819e9075deea490dd05ec61f46062
Stefan Hoffmann <stho32github@gmail.com>
2018-12-09 09:50:23 +0100

    New Cmdlet to add PSCore Tools to profile

---
+++ Setup/Add-PSCToProfile.ps1
@@ -0,0 +1,21 @@
+﻿function Add-PSCoreToolsToProfile {
+    <#
+        .SYNOPSIS
+        Adds this library to the current users profile, so all commandlets are loaded at startup.
+
+        .EXAMPLE
+        Add-PSCToProfile
+
+        Adds this library to your profile. Creates the profile file if it does not exist.
+    #>
+    [CmdletBinding()]
+    param ()
+    
+    process {
+        if ( -not (Test-Path $profile) ) {
+            New-Item $profile -ItemType File -Force
+        }
+
+        "`n. $PSScriptRoot/PSCoreTools.ps1" | Out-File $profile -Append
+    }    
+}
