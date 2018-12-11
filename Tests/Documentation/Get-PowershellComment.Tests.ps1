48481c3ab2d0637658a7fedb14d4c2f23d10097f
Stefan Hoffmann <Stefan.Hoffmann@mahr-edv.de>
2018-12-11 22:17:27 +0100

    We have now a cmdlet that can read the multiline comments from powershell files

--- Tests/Documentation/Get-PowershellComment.Tests.ps1
+++ Tests/Documentation/Get-PowershellComment.Tests.ps1
@@ -1,7 +1,10 @@
 ﻿Describe 'Get-PowershellComment' {
     It "grabs all comments from Get-CleanRepository" {
-        $comments = Get-PowershellComment -FilePath "../../Source/VersionControl/Get-CleanRepository.ps1"
+        $path = Join-Path $PSScriptRoot "../../Source/VersionControl/Get-CleanRepository.ps1"
+        $comments = Get-PowershellComment -FilePath $path
 
         $comments | Should -Not -Be $null
+        ($comments | Measure-Object).Count | Should -Be 1
+        $comments | Should -BeLike "*SYNOPSIS*"
     }
 }
